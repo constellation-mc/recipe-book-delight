b6bb9384c6ff430c1d5a5026f0a2af4b19614342
melontini <104443436+melontini@users.noreply.github.com>
2023-09-12 21:29:51 +0700

    Added `recipe-book-delight:registered_groups` to ObjectShare.

--- src/main/java/me/melontini/recipe_book_delight/client/RecipeBookDelightClient.java
+++ src/main/java/me/melontini/recipe_book_delight/client/RecipeBookDelightClient.java
@@ -6,11 +6,13 @@ import com.nhoryzon.mc.farmersdelight.registry.RecipeTypesRegistry;
 import me.melontini.dark_matter.api.recipe_book.RecipeBookHelper;
 import me.melontini.recipe_book_delight.RecipeBookDelight;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
 public class RecipeBookDelightClient implements ClientModInitializer {
+
     public static final RecipeBookGroup COOKING_POT_SEARCH = RecipeBookHelper.createGroup(new Identifier("recipe_book_delight", "search"), Items.COMPASS.getDefaultStack());
     public static final RecipeBookGroup COOKING_POT_MAIN = RecipeBookHelper.createGroup(new Identifier("recipe_book_delight", "main"), ItemsRegistry.FRIED_RICE.get().getDefaultStack());
 
@@ -19,5 +21,7 @@ public class RecipeBookDelightClient implements ClientModInitializer {
         RecipeBookHelper.addRecipePredicate(RecipeTypesRegistry.COOKING_RECIPE_SERIALIZER.type(), recipe -> COOKING_POT_MAIN);
         RecipeBookHelper.addToGetGroups(RecipeBookDelight.COOKING_POT, Lists.newArrayList(COOKING_POT_SEARCH, COOKING_POT_MAIN));
         RecipeBookHelper.addToSearchMap(COOKING_POT_SEARCH, Lists.newArrayList(COOKING_POT_MAIN));
+
+        FabricLoader.getInstance().getObjectShare().put("recipe-book-delight:registered_groups", RecipeBookDelight.COOKING_POT);
     }
 }
